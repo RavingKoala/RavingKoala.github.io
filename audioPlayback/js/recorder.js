2495129057abf94301e97dec957244a93dfc5ac1
RavingKoala <stijnvb@hotmail.com>
2022-06-24 18:48:16 +0200

    updated code standards

--- audioPlayback/js/recorder.js
+++ audioPlayback/js/recorder.js
@@ -12,17 +12,18 @@ document.addEventListener(RecorderEvents.onEnded, () => { document.dispatchEvent
 class Recorder {
 	isRecording
 	isEnded
-	audioRecorder
-	mediaStream
-	audioObj
+	#playASAP
+	#audioRecorder
+	#mediaStream
+	#audioObj
 	constructor () {
 		this.isRecording = false
 		this.isEnded = true
-		this.mediaStream
+		this.#mediaStream
 		this.lastBlobRaw
-		this.audioObj = new Audio()
+		this.#audioObj = new Audio()
 
-		this.audioObj.onpause = () => {
+		this.#audioObj.onpause = () => {
 			this.isEnded = true
 			document.dispatchEvent(new Event(RecorderEvents.onEnded))
 		}
@@ -37,21 +38,21 @@ class Recorder {
 		const url = window.URL.createObjectURL(audioBlob)
 		// history
 		// blobHistory.push(audioBlob) // option for later
-		this.audioObj.src = url
+		this.#audioObj.src = url
 		// Tear down after recording.
-		this.audioRecorder.stream.getTracks().forEach(t => t.stop())
-		this.audioRecorder = null
+		this.#audioRecorder.stream.getTracks().forEach(t => t.stop())
+		this.#audioRecorder = null
 		// play recording
 		this.isEnded = false
 		// TODO setting make this inbetween step
-		this.audioObj.play()
+		this.#audioObj.play()
 	}
 
 	async requestPerms() {
 		await window.navigator.mediaDevices.getUserMedia(
 			{ audio: true }
 		).then((stream) => {
-			this.mediaStream = stream
+			this.#mediaStream = stream
 
 			var event = RecorderEvents.onRecordPermsUpdate
 			event.detail.IsRecAllowed = true
@@ -66,9 +67,9 @@ class Recorder {
 	}
 
 	setupRecorder() {
-		this.audioRecorder = new MediaRecorder(this.mediaStream)
+		this.#audioRecorder = new MediaRecorder(this.#mediaStream)
 
-		this.audioRecorder.ondataavailable = (data) => {
+		this.#audioRecorder.ondataavailable = (data) => {
 			this.processData(data)
 			// play recording
 			document.dispatchEvent(RecorderEvents.onReady)
@@ -78,14 +79,14 @@ class Recorder {
 	async startRecording() {
 		await this.requestPerms()
 		this.setupRecorder()
-		this.audioRecorder.start()
+		this.#audioRecorder.start()
 		this.isRecording = true
 	}
 
 	stopRecording() {
 		// TODO setting make this optional
 		// await new Promise(res => setTimeout(res, 500))
-		this.audioRecorder.stop()
+		this.#audioRecorder.stop()
 		this.isRecording = false
 	}
 
@@ -94,6 +95,6 @@ class Recorder {
 	}
 
 	stopPlaying() {
-		this.audioObj.pause()
+		this.#audioObj.pause()
 	}
 }
